b02b89c58cf611cddf2d8e912d4bf08a674255dc
Edwin Ligorio Salgado <lesf@miflink.com>
2017-04-18 21:00:01 -0500

    first commit

---
+++ landing/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    saludo = "Hola me llamo Edwin!"
+    usuarios = [
+        {"nombre":"Edwin","edad":23,"apellidos":"Salgado"},
+        {"nombre":"Alfonso","edad":19,"apellidos":"Torres","escolaridad":"licenciatura"},
+        {"nombre":"Luis","edad":27,"apellidos":"Perez"},
+
+    ]
+    return render(request,"landing/index.html",{
+        "greeting":saludo,
+        "users":usuarios
+        
+        })
